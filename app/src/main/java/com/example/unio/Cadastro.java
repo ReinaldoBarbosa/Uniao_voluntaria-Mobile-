a2b81c4fbbf0dc2992d513c9b9b9070d9d68c56c
ReinaldoBarbosa <reinaldobarbosa034@gmail.com>
2024-06-25 08:24:23 -0300

    Atualizações na tela de cadastro

--- app/src/main/java/com/example/unio/Cadastro.java
+++ app/src/main/java/com/example/unio/Cadastro.java
@@ -3,6 +3,9 @@ package com.example.unio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import Model.SquareView;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
+        SquareView squareView = new SquareView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(200, 200);
+        squareView.setLayoutParams(params);
+        LinearLayout layout = findViewById(R.id.layout_cadastro);
+        layout.addView(squareView);
     }
 }
--- app/src/main/java/com/example/unio/MainActivity.java
+++ app/src/main/java/com/example/unio/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-   }
+
+
+
+
+    }
 }
